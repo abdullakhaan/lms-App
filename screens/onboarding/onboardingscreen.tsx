75fe442323cb49c4dc7910a323d8ea0007fd8bb7
Muhammad Abdullah Khan <abdullah.khana7993@gmail.com>
2024-12-11 12:18:52 +0500

    done till 35:17

--- screens/onboarding/onboardingscreen.tsx
+++ screens/onboarding/onboardingscreen.tsx
@@ -54,7 +54,7 @@ export default function onboardingscreen() {
             source={require("@/assets/onboarding/shape_6.png")}
             />
             <Text style={[styles.titleText, {fontFamily:"raleway_700Bold"}]}>
-              Becodemy
+              DeepSense
 
             </Text>
 

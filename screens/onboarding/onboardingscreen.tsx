28f0edfed471f0a1cc84e1def359c643db4a1883
Muhammad Abdullah Khan <abdullah.khana7993@gmail.com>
2024-12-12 11:48:46 +0500

    some chnages

--- screens/onboarding/onboardingscreen.tsx
+++ screens/onboarding/onboardingscreen.tsx
@@ -60,7 +60,7 @@ export default function onboardingscreen() {
             Explore a variety of interactive lesson, 
           </Text>
           <Text style={[styless.dscpText, {fontFamily: "Nunito_400Regular"}]}>
-           video, quizzes & assignment.
+           video, quizzes & assignments.
           </Text>
         </View>
 

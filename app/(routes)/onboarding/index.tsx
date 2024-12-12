3db7097e3b03088c349113c0713bb00d7add1d1e
Muhammad Abdullah Khan <abdullah.khana7993@gmail.com>
2024-12-12 11:59:13 +0500

    folder made

--- app/(routes)/onboarding/index.tsx
+++
@@ -1,8 +0,0 @@
-import OnBoardingScreen from "@/screens/onboarding/onboardingscreen";
-
-export default function OnBoarding() {
-  return (
-    <OnBoardingScreen />
-  )
-}
-
--- types/global.d.ts
+++
@@ -1,9 +0,0 @@
-type onboardingSwiperDataType = {
-    id: number;
-    title: string;
-    description: string;
-    sortDescrition: string;
-    sortDescrition2?: string;
-    image: any;
-
-};

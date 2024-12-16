96b841a13d3016e081d9ce9b6840d42207023b0b
M-Hasssan <muhammed.hassan.tariq@gmail.com>
2024-12-16 22:35:36 +0500

    server addedd

---
+++ server/app.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from "express";
+require('dotenv').config();
+export const app = express();
+import cookieParser from "cookie-parser";
+import  cors  from "cors";
+
+// body parcer
+
+app.use(express.json({limit: "50mb"}));
+
+// cookie parcer
+
+ app.use(cookieParser());
+
+ // cors => cross origin resource sharing 
+ app.use(cors({
+    origin: process.env.ORIGIN
+ }));
+
+ // TESTING API 
+
+ app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "API is working",
+    })
+ });  
+
+
+// unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+  });

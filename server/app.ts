8bde31a3da919d55663e70ef41c306545aecbb14
M-Hasssan <muhammed.hassan.tariq@gmail.com>
2024-12-19 22:21:01 +0500

    user registration and activation

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ export const app = express();
 import cookieParser from "cookie-parser";
 import  cors  from "cors";
 import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 // body parcer
 
@@ -17,6 +18,13 @@ app.use(express.json({limit: "50mb"}));
  app.use(cors({
     origin: process.env.ORIGIN
  }));
+
+ // routes
+ 
+ app.use(
+   "/api/v1",
+   userRouter
+ );
 
  // TESTING API 
 
